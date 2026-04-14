67923130c8163f0648c23382d7d3faad6d151fa9
Eric Leocadio <ericleocadio9@gmail.com>
2026-04-14 19:16:58 -0400

    adding DB and supabase connection

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
 import json
 from flask import Flask, jsonify, request
-
+from supabase import create_client
 app = Flask(__name__)
 
-sampleData = {"language": "en", "transcription": "hə'loʊ"}
+#Connect to Supabase DB
+SUPABASE_URL = "https://elyqlcqcmkvvrakgzvtc.supabase.co"
+SUPABASE_KEY = "sb_publishable_JX5ruW4jnO0j-e05C3L0KQ_dsooRy9_"
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
+#DB functions
+
+def read_words():
+    return supabase.rpc('ReadWordDB').execute()
+
+def read_IPA():
+    return supabase.rpc('ReadIPA').execute()
+
+def insert_to_db(scraped_word):
+    supabase.rpc("Insert_word", {"scraped_word":scraped_word}).execute()
+
+#scrape function
 def webScrape(lang, result):
     pass
 
+#dummy data
+sampleData = {"language": "en", "transcription": "hə'loʊ"}
 
+#api route
 @app.route('/api', methods=['GET'])
 def get_data():
-    lang = request.args.get('lang', 'en')
+    lang = request.args.get('lang','en')
     transcript = request.args.get("word", "placeholder")
-    result = webScrape(lang, transcript)
-
-    return jsonify(sampleData)
+    scraped_data = webScrape(lang, transcript)
+    insert_to_db(sampleData)
+    return jsonify(sampleData) #REPLACE WITH REAL DATA
